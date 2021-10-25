6e6f4d836e331385fe50a221af6483d76739b416
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 14:25:49 -0600

    Function Intermediate #1 Corrected

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -13,6 +13,11 @@ z = [ {'x': 10, 'y': 20} ]
 x[1][0] = 15
 print(x)
 
-#2
 students[0]['last_name'] = "Bryant"
 print(students[0])
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z[0])
