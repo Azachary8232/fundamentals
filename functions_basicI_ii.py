d5564aa5055b6984d25e3fa939e0fd86f3e8a834
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 12:06:22 -0600

    Functions Basics II #3

--- functions_basicI_ii.py
+++ functions_basicI_ii.py
@@ -9,13 +9,17 @@ def countdown(num):
 
 *newList = countdown(5)
 print(newList)
-"""
+
 #2
 def print_and_return(list):
     print(list[0])
     return(list[1])
 
 print(print_and_return([1,2]))
-
+"""
 #3
-def
+def first_plus_length(list):
+    sum = list[0] + len(list)
+    return sum
+
+print(first_plus_length([1,2,3,4,5]))
