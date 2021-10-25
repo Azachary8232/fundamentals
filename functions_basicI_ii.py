982d48cfdf4bee8360eb5a7cf004d892de78b241
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 12:51:41 -0600

    Functions Basics II #5

--- functions_basicI_ii.py
+++ functions_basicI_ii.py
@@ -23,7 +23,7 @@ def first_plus_length(list):
     return sum
 
 print(first_plus_length([1,2,3,4,5]))
-"""
+
 #4
 #function that creates a 'New List' with values greater than 2nd value
 #print amount of values greater than 2nd
@@ -43,4 +43,13 @@ def values_greater_than_second(list):
 
 print(values_greater_than_second([5,2,3,2,1,4]))
 print(values_greater_than_second([5,4,3,2,1,4]))
+"""
+#5
+def length_and_value(length, value):
+    new_list = []
+    for x in range(length):
+        new_list.append(value)
+    return new_list
 
+print(length_and_value(4,7))
+print(length_and_value(6,2))
