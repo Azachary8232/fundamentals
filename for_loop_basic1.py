15ad86db7c68fcd008b453eda4a7046c56ab1686
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 12:18:58 -0600

    For Loops Basic I #3

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -7,6 +7,14 @@ for x in range(151):
 for x in range(5,151,5):
     print(x)
 
+#3
+for x in range(1,101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 10 != 0 and x % 5 == 0:
+        print("Coding")
+    else:
+        print(x)
 
 
 
