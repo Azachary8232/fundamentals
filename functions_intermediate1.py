04a03ed57448a0547404fa0a8f689c38d392a03b
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 17:14:44 -0600

    Functions Intermediate 1 Complete

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -1,8 +1,8 @@
-"""
+
 x = [ [5,2,3], [10,8,9] ] 
 students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
 ]
 sports_directory = {
     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
@@ -22,21 +22,21 @@ print(sports_directory['soccer'])
 
 z[0]['y'] = 30
 print(z[0])
-"""
+
 #2
 students = [
-         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-         {'first_name' : 'John', 'last_name' : 'Rosales'},
-         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
     ]
-"""
+
 def iterate_dictionary(list):
     for x in range(0, len(list)):
         print(list[x])
 
 iterate_dictionary(students)    
-"""
+
 #3
 def iterate_dictionary2(key_name, some_list):
     for x in range(0, len(some_list)):
@@ -44,3 +44,32 @@ def iterate_dictionary2(key_name, some_list):
 
 iterate_dictionary2('first_name', students)    
 iterate_dictionary2('last_name', students)    
+
+#4
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info2(some_dict2):
+    for key in some_dict2:
+        print(len(some_dict2[key]), key.upper())
+        for items in some_dict2[key]:
+            print(items)
+        print()
+
+print_info2(dojo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
