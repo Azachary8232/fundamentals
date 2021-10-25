ce4c76deb0026fba6352815ab65559a9923e3fe7
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-25 12:07:29 -0600

    Functions Basics II #4

--- functions_basicI_ii.py
+++ functions_basicI_ii.py
@@ -16,10 +16,31 @@ def print_and_return(list):
     return(list[1])
 
 print(print_and_return([1,2]))
-"""
+
 #3
 def first_plus_length(list):
     sum = list[0] + len(list)
     return sum
 
 print(first_plus_length([1,2,3,4,5]))
+"""
+#4
+#function that creates a 'New List' with values greater than 2nd value
+#print amount of values greater than 2nd
+#if list has < 2 elements return False
+def values_greater_than_second(list):
+    new_list = []
+    count = 0
+    for i in range(0, len(list)):
+        if list[i] > list[1]:
+            count = count + 1
+            new_list.append(list[i])
+    if count < 2:
+        return False
+    else:
+        print(count)
+        return new_list           
+
+print(values_greater_than_second([5,2,3,2,1,4]))
+print(values_greater_than_second([5,4,3,2,1,4]))
+
